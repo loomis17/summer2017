b71b48c975589f59bc566472f9dfa522279f09a3
Pablo Santos <pabloxsantos@gmail.com>
2017-08-23 22:34:54 -0700

    Twitter access token added

--- summer2017/summer2017/ViewController.swift
+++ summer2017/summer2017/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Twitter Access Token: 789737662747860992-au3KBk0zPSa4HvSsWSfiJ3AUDekyWkc
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
